98a6e18c83020cf00b0bd366675e6a1ba3694ecc
pritee-bit <priteeranjan911@gmail.com>
2021-10-16 01:57:54 +0530

    Init prisma

--- server.js
+++ server.js
@@ -6,78 +6,100 @@ const server = express()
 
 server.use(express.json())
 
-let bList = []
+const mysql = require("mysql2");
 
-server.get('/', (req, res) => {
-    res.send('your library')
-})
+var conn = mysql.createConnection({ host: "localhost", user: "root", password: "12345", database: "your_library" });
 
-server.get('/addbook', (req, res) => {
-    res.send('')
-})
 
-server.post('/savebook', (req, res) => {
-    let bname = req.body.bname;
-    let newbookid = crypto.randomUUID();
-    const book = { id: newbookid, name: bname };
-    bList.push(book);
-    res.json({ ...book, message: 'book has been added' })
-})
+let initdb = () => {
+    conn.query("CREATE TABLE book (id VARCHAR(40) PRIMARY KEY, name VARCHAR(100))", (err, result) => {
+        if (err && err.code!== 'ER_TABLE_EXISTS_ERROR') throw err;
+        
+        let bList = []
 
-server.post('/deletebook', (req, res) => {
-    let del = req.body.id
-    let delb = null
-    bList = bList.filter((val, index) => {
-        if (val.id == del) {
-            delb = val
-            return false;
-        }
-        return true;
+        server.get('/', (req, res) => {
+            res.send('your library')
+        })
+
+        server.get('/addbook', (req, res) => {
+            res.send('')
+        })
+
+        server.post('/savebook', (req, res) => {
+            let name = req.body.bname;
+            let id = crypto.randomUUID();
+            const book = { id, name };
+            conn.query(`insert into book VALUES("${id}","${name}")`,(err,resl)=>{
+                console.log(resl,err)
+                if (err) res.json({message: 'an error occured, can\'t save book.'})
+                else res.json({ ...book, message: 'book has been added' })
+                
+            })
+
+        })
+
+        server.post('/multibook', (req, res) => {
+            let bnames = req.body.names;
+            bnames.forEach(name => {
+                let newbookid = crypto.randomUUID();
+                const book = { id: newbookid, name: name };
+                bList.push(book);
+            });
+            res.json({ message: 'all books are added.' })
+        })
+
+        server.post('/deletebook', (req, res) => {
+            let del = req.body.id
+            let delb = null
+            conn.query(`DELETE FROM book WHERE id="${del}" `,(err)=>{
+                console.log(err)
+                if (err) res.json({message: 'an error occured, can\'t delete book.'})
+                else res.json({message: 'book has been deleted' })
+            })
+        })
+
+
+        server.post('/multidelete', (req, res) => {
+            const mdel = req.body.ids;
+            let list = bList;
+            mdel.forEach((id) => {
+                list = list.filter((book) => book.id != id)
+            })
+            const num = bList.length - list.length
+            bList = list
+            res.json({ message: "books have been deleted", total_deleted: num })
+        })
+
+        server.get('/viewbook/:bid', (req, res) => {
+            let bid = req.params.bid
+            let viewB = bList.filter((val, index) => val.id == bid);
+            if (!viewB.length) {
+                return res.json({ message: 'book does not exist' })
+            }
+            res.json(viewB[0])
+        })
+
+        server.get('/viewall', (req, res) => {
+            res.json(bList)
+        })
+
+        
+
+        const port = 3000;
+        server.listen(port, 'localhost', () => {
+            console.log(port)
+            console.log("Server started, honey");
+        })
+
+
     })
-    if (delb) {
-        return res.json({ ...delb, message: 'book has been deleted.' })
-    }
-    res.json({ message: "book does not exist" })
-})
+
+}
+const afterConnect = (err) => {
+    if (err) throw err
+    console.log("connected")
+    initdb()
+}
+conn.connect(afterConnect)
 
 
-server.post('/multidelete', (req, res) => {
-    const mdel = req.body.ids;
-    let list = bList;
-    mdel.forEach((id) => {
-        list = list.filter((book) => book.id != id)
-    })
-    const num = bList.length - list.length
-    bList = list
-    res.json({ message: "books have been deleted", total_deleted: num })
-})
-
-server.get('/viewbook/:bid', (req, res) => {
-    let bid = req.params.bid
-    let viewB = bList.filter((val, index) => val.id == bid);
-    if (!viewB.length) {
-        return res.json({ message: 'book does not exist' })
-    }
-    res.json(viewB[0])
-})
-
-server.get('/viewall', (req, res) => {
-    res.json(bList)
-})
-
-server.post('/multibook', (req, res) => {
-    let bnames = req.body.names;
-    bnames.forEach(name => {
-        let newbookid = crypto.randomUUID();
-        const book = { id: newbookid, name: name };
-        bList.push(book);
-    });
-    res.json({ message: 'all books are added.' })
-})
-
-
-const port = 3000;
-server.listen(port, 'localhost', () => {
-    console.log(port)
-    console.log("Server started, honey");
-})
